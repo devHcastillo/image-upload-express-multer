19054934ac0ebb21bd1a61bf3f0da09c2f4e0838
Heraldo Castillo <dev.hcastillo@gmail.com>
2021-02-01 11:51:10 -0800

    full upload image test

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,16 +1,57 @@
+const uploadModel = require("../model/schema");
+const fs = require("fs");
 
+exports.home = async (req, res) => {
+  const all_images = await uploadModel.find();
+  res.render("main", { images: all_images });
+};
 
-exports.home = (req, res) => {
-    res.render("main");
-  };
-
-  exports.uploads = (req, res, next) => {
-    const files = req.files;
-    if(!files){
-      const error = new Error('Upload some image')
-      error.httpStatusCode = 400
-      return next(error)
-    }
-
-    res.json(files)
+exports.uploads = (req, res, next) => {
+  const files = req.files;
+  if (!files) {
+    const error = new Error("Upload some image");
+    error.httpStatusCode = 400;
+    return next(error);
   }
+
+  //convert images into base64 encoding
+  let imgArray = files.map((file) => {
+    let img = fs.readFileSync(file.path);
+    return (encode_image = img.toString("base64"));
+  });
+
+  let result = imgArray.map((src, index) => {
+    //create object to store data in the collection
+    let finalimg = {
+      filename: files[index].originalname,
+      contentType: files[index].mimetype,
+      imageBase64: src,
+    };
+    let newUpload = new uploadModel(finalimg);
+    return newUpload
+      .save()
+      .then(() => {
+        return {
+          msg: `${files[index].originalname} image Uploaded Successfully`,
+        };
+      })
+      .catch((error) => {
+        if (error) {
+          if (error.name === "MongoError" && error.code === 11000) {
+            return Promise.reject({
+              error: `Duplicate ${files[index].originalname} `,
+            });
+          }
+          return Promise.reject({ error: error.message || `Cannot Upload` });
+        }
+      });
+  });
+
+  Promise.all(result)
+    .then((msg) => {
+      res.redirect("/");
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+};
